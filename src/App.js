c826131e36c0afe012b769ad3a916b8afe186d2f
iaradocampo <docampo.iara@gmail.com>
2022-01-06 15:54:56 -0300

    correccion

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import ItemListContainer from './container/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Cart from './components/cart/Cart';
-import Order from './components/order/Order';
+import Order from './components/order/Order'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/app.scss';
 
